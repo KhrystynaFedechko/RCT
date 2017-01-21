9d02a7ffb13598ce033f09d0fb403c1e33c0a337
khrystyna13 <khrystynadyka@gmail.com>
2017-01-21 14:19:13 +0200

    add all frontend files

---
+++ js/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by User on 24.12.2016.
+ */
+$(function () {
+    $('#btnLoginSubmit').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "api/user/get-spring-user",
+            method: "GET",
+            data: {
+                userName: $('#loginName').val(),
+                password: $('#pwd').val()
+            },
+            dataType: 'json'
+        })
+            .done(function (response) {
+                if ( response.userRole == "ROLE_REQUESTOR") {
+                    window.location = "api/home/requestor";
+                }
+            })
+            .fail(function (response) {
+                // alert(response.responseText);
+                $('#invalid').html('<p>Invalid user name or password.</p>');
+            })
+    })
+});
