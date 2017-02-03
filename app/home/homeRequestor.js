de1071d8df7cea16c42b594da14021b46354e2db
khrystyna13 <khrystynadyka@gmail.com>
2017-02-03 17:10:10 +0200

    add js file

--- app/home/homeRequestor.js
+++
@@ -1,4 +0,0 @@
-// function backToLoginPage() {
-//     window.location = "http://localhost:8080/rma-tool/";
-console.log("Hi loser =)");
-// }
---
+++ js/homeRequestor.js
@@ -0,0 +1,3 @@
+function backToLoginPage() {
+    window.location = "../";
+}
